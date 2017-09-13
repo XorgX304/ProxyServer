80d4074d6bb5639c970fc5e69b3ae7f2763ce0e3
吕途 <buzhidaolvtu@163.com>
2017-09-13 15:06:23 +0800

    release resource

--- ProxyServer/src/main/java/org/game/throne/proxy/forward/relation/RelationKeeper.java
+++ ProxyServer/src/main/java/org/game/throne/proxy/forward/relation/RelationKeeper.java
@@ -3,18 +3,46 @@ package org.game.throne.proxy.forward.relation;
 import io.netty.channel.ChannelHandlerContext;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 /**
  * Created by lvtu on 2017/9/7.
  */
-public class RelationKeeper {
+public class RelationKeeper implements Runnable {
 
     private List<Relation> relations = new ArrayList<>();
 
     private ReentrantLock lock = new ReentrantLock();
+
+    private ScheduledThreadPoolExecutor housekeeper = new ScheduledThreadPoolExecutor(1);
+
+    public RelationKeeper() {
+        housekeeper.scheduleWithFixedDelay(this, 0, 5, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 释放已经断开的channel relation内存资源,让gc回收channnel ctx对象占用的内存。
+     */
+    @Override
+    public void run() {
+        try {
+            lock.lock();
+            Iterator<Relation> iterator = relations.iterator();
+            while (iterator.hasNext()) {
+                Relation relation = iterator.next();
+                if (relation.breaked()) {
+                    iterator.remove();
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
 
     public void addRelation(ChannelHandlerContext left, ChannelHandlerContext right) {
         try {
